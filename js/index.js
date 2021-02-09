681fa82f2a17ccef658651f1cbc1e9b54cb0a17a
vickitvle <vickitvle@gmail.com>
2021-02-09 16:12:26 -0500

    edits

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$("#mydivheader").on("click", function(){
+  $("#dropDown").slideToggle();
+});
+
+window.onscroll = function() {
+  menu();
+  scroll();
+}
+
+function menu() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    $('#mydiv').fadeIn();
+
+  } else {
+    $('#mydiv').fadeOut();
+  }
+}
